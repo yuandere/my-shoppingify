947879c0e62dfd9082245a614fb55b4f216238f2
Derek <derekylee@outlook.com>
2023-06-03 23:59:11 -0700

    list item checked item ux, qty update

--- src/components/listItem.tsx
+++ src/components/listItem.tsx
@@ -5,12 +5,45 @@ import { IListItem } from '@/@types/dashboard';
 export default function ListItem({ listItem }: { listItem: IListItem }) {
 	const [isEditingListItem, setIsEditingListItem] = useState<boolean>(false);
 	const [isChecked, setIsChecked] = useState<boolean>(false);
+	const [itemQuantity, setItemQuantity] = useState<number>(0);
 	const cartStates = useContext(CartStatesContext);
 	const listItemRef = useRef<HTMLDivElement>(null);
+	const checkboxRef = useRef<HTMLInputElement>(null);
+
+	// TODO: add logic to update checked
+	const handleListItemClick = () => {
+		if (cartStates?.isCartEditingState) {
+			if (!isEditingListItem) {
+				setIsEditingListItem(true);
+			} else {
+				setIsEditingListItem(false);
+			}
+		} else if (checkboxRef.current) {
+			checkboxRef.current.click();
+		}
+	};
+
+	const handleQuantityChange = (add: boolean) => {
+		if (add && itemQuantity < 99) {
+			setItemQuantity(itemQuantity + 1);
+		} else if (!add && itemQuantity > 1) {
+			setItemQuantity(itemQuantity - 1);
+		}
+	};
+
+	const updateQuantityReq = () => {
+		// check for changes to list copy before API call?
+		console.log('API req to update quantity called');
+	};
+
+	useEffect(() => {
+		setItemQuantity(listItem.quantity);
+	}, [listItem.quantity]);
 
 	useEffect(() => {
 		if (!cartStates?.isCartEditingState && isEditingListItem) {
 			setIsEditingListItem(false);
+			updateQuantityReq();
 		}
 	}, [cartStates?.isCartEditingState, isEditingListItem]);
 
@@ -22,6 +55,7 @@ export default function ListItem({ listItem }: { listItem: IListItem }) {
 				!listItemRef.current.contains(event.target as Node)
 			) {
 				setIsEditingListItem(false);
+				updateQuantityReq();
 			}
 		};
 		document.addEventListener('mousedown', handleClickOutside);
@@ -30,32 +64,28 @@ export default function ListItem({ listItem }: { listItem: IListItem }) {
 		};
 	}, [listItemRef, isEditingListItem]);
 
-	// TODO add logic to compare new quanitity against old before updating
 	return (
 		<div
-			className={`flex items-center justify-between -mb-2 py-1 px-2 w-full rounded-lg bg-theme-3 hover:drop-shadow-[0_2px_9px_rgba(0,0,0,0.14)]${
-				cartStates?.isCartEditingState ? ' cursor-pointer' : null
-			}`}
+			className={`flex items-center justify-between -mb-2 py-1 px-2 w-full rounded-lg bg-theme-3 hover:drop-shadow-[0_2px_9px_rgba(0,0,0,0.14)] cursor-pointer`}
 			onClick={() => {
-				if (cartStates?.isCartEditingState && !isEditingListItem) {
-					setIsEditingListItem(true);
-				} else {
-					setIsEditingListItem(false);
-				}
-				console.log('div clicked, item editing:', isEditingListItem);
+				handleListItemClick();
 			}}
 			ref={listItemRef}
 		>
 			{cartStates?.isCartEditingState ? null : (
 				<input
+					ref={checkboxRef}
 					type='checkbox'
 					className='accent-theme-1 bg-white w-4 h-4 cursor-pointer'
+					checked={isChecked ? true : false}
 					onChange={(e) => {
 						console.log('checkbox clicked');
 						if (e.target.checked === true) {
 							setIsChecked(true);
+							console.log('req to check');
 						} else {
 							setIsChecked(false);
+							console.log('req to uncheck');
 						}
 					}}
 					onClick={(e) => {
@@ -84,17 +114,19 @@ export default function ListItem({ listItem }: { listItem: IListItem }) {
 						className='material-icons m-1 text-theme-1 cursor-pointer transition select-none text-xl hover:text-orange-600'
 						onClick={(e) => {
 							e.stopPropagation();
+							handleQuantityChange(false);
 						}}
 					>
 						remove
 					</span>
 					<div className='grid place-items-center w-14 h-7 rounded-full text-xs font-medium border-2 border-theme-1 select-none'>
-						<p className='text-theme-1'>{listItem.quantity} pcs</p>
+						<p className='text-theme-1'>{itemQuantity} pcs</p>
 					</div>
 					<span
 						className='material-icons m-1 text-theme-1 cursor-pointer transition select-none text-xl hover:text-orange-600'
 						onClick={(e) => {
 							e.stopPropagation();
+							handleQuantityChange(true);
 						}}
 					>
 						add
@@ -102,7 +134,7 @@ export default function ListItem({ listItem }: { listItem: IListItem }) {
 				</div>
 			) : (
 				<div className='grid place-items-center w-14 h-7 rounded-full text-xs font-medium border-2 border-theme-1'>
-					<p className='text-theme-1'>{listItem.quantity} pcs</p>
+					<p className='text-theme-1'>{itemQuantity} pcs</p>
 				</div>
 			)}
 		</div>
