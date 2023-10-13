ab1a0d34bfc45d017b02c2895e74e59a2e1deb87
Derek <derekylee@outlook.com>
2023-10-12 23:52:49 -0700

    implement nextauth session

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,17 @@
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../api/auth/[...nextauth]/route';
 import { Providers } from './providers';
+import { preload } from '@/lib/getItems';
+import 'server-only'
 
 export default async function DashboardLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	return <Providers>{children}</Providers>;
+	const session = await getServerSession(authOptions);
+	if (session?.user.id) {
+		preload(session.user.id);
+	}
+	return <Providers session={session}>{children}</Providers>;
 }
